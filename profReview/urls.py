788f6b347b2ad4d6b456a2615b75fa8fb5486b8a
Vishal Asthana <58475949+CrypticPoet@users.noreply.github.com>
2020-03-26 23:42:11 +0530

    Added a registration page

--- profReview/urls.py
+++ profReview/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as users_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', users_view.register, name='Register'),
     path('', include('reviews.urls')),
 ]
---
+++ users/views.py
@@ -0,0 +1,21 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from .forms import SignUpForm
+
+def register(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'users/register.html', {'form': form})
+
+# Create your views here.
