226c34c086440278ec6144eb2cb8b1d7299938eb
Vishal Asthana <58475949+CrypticPoet@users.noreply.github.com>
2020-03-26 20:26:35 +0530

    Added course page

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='main-home'),
     path('profs/', views.prof_home, name='Professors-home'),
+    path('courses/', views.course_home, name='courses-home'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -2,11 +2,6 @@ from django.shortcuts import render
 from .models import Professor, Course
 from django.http import HttpResponse
 
-# professors = [{'name':'Prof. Ajit Kumar', 'department':'Applied Mechanics', 'courses':'APL100'},
-#               {'name':'Prof. Subhasish Bannerjee', 'department':'Computer Science', 'courses':'COL100'},
-#               {'name':'Prof. Bhim Singh', 'department':'Electrical Engineering', 'courses':'ELL100'}
-#              ]
-
 def home(request) :
     return render(request, 'reviews/home.html')
 
@@ -16,4 +11,4 @@ def prof_home(request) :
 
 def course_home(request) :
     context = {'courses':Course.objects.all(), 'title':'Courses'}
-    return render(request, 'reviews/courses/html', context)
+    return render(request, 'reviews/courses.html', context)
