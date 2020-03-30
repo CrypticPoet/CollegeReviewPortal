d193e1c54080514c962a86df765031b4fdb1141e
Vishal Asthana <58475949+CrypticPoet@users.noreply.github.com>
2020-03-30 23:57:29 +0530

    Added option to sort reviews by number of reports in the Admin Panel

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db.models import Count
+
 from .models import Professor, Course, Prof_review, Course_review
 
 class ProfessorAdmin(admin.ModelAdmin):
@@ -8,10 +10,34 @@ class CourseAdmin(admin.ModelAdmin):
     list_display = ('name', 'overall_rating')
 
 class ProfReviewAdmin(admin.ModelAdmin):
-    list_display = ('__str__','total_reports',)
+    list_display = ('__str__', 'author', 'prof','report_count',)
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(
+            _report_count=Count("reports", distinct=True),
+        )
+        return queryset
+
+    def report_count(self, obj):
+        return obj._report_count
+    report_count.admin_order_field = '_report_count'
+    report_count.short_description = 'Reports'
 
 class CourseReviewAdmin(admin.ModelAdmin):
-    list_display = ('__str__','total_reports',)
+    list_display = ('__str__', 'author', 'course', 'report_count')
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(
+            _report_count=Count("creports", distinct=True),
+        )
+        return queryset
+
+    def report_count(self, obj):
+        return obj._report_count
+    report_count.admin_order_field = '_report_count'
+    report_count.short_description = 'Reports'
 
 
 admin.site.register(Professor, ProfessorAdmin)
